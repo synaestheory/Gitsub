5339fb3fa625d0110bcde25ec6c9f6b1e3aec78b
Seth Niimi <seth.niimi@gmail.com>
2016-04-24 22:33:14 -0700

    Switch to Lodash - Refactor most anonymous functions to be named - Add more verbose messaging

--- gitsub.js
+++ gitsub.js
@@ -5,87 +5,92 @@ var Promise     = require('bluebird'),
     fs          = Promise.promisifyAll(require('graceful-fs')),
     path        = require('path'),
     exec        = require('child-process-promise').exec,
-    _           = require('underscore'),
+    _           = require('lodash/fp'),
     chalk       = require('chalk');
 
+var log = console.log.bind(console);
+function errLog() {
+    log(chalk.red.apply([].prototype.slice.call(arguments)));
+}
+var logStderr = _.flow(_.property('stderr'), errLog);
 var flags = process.argv.filter(function(v) {return v.indexOf('--') === 0;});
-const BRANCH_FLAG = flags.length > 0 ? flags[0].replace('--', '') : 'master';
+const BRANCH_FLAG = flags.length > 0 ? flags[0].replace('--', '') : null;
 
-
+console.log('Performing \'git pull\' and \'git submodule update\'');
 console.time(chalk.green('git submodule sync'));
-var cmd = 'git pull && git submodule update --init --recursive';
-exec(cmd)
-.catch(function(error) {
-    console.log(error.stderr);
-})
-.then(function(result) {
+exec('git pull && git submodule update --init --recursive')
+.catch(logStderr)
+.then(submoduleUpdateSuccessHandler);
+
+function submoduleUpdateSuccessHandler(result) {
     if(typeof result.stderr != 'undefined') {
         console.log(result.stderr);
         syncModules();
         return {status: 'success', message: result.stderr};
     }
     syncModules();
-});
+}
+
+function submodulePullErrorHandler(error) {
+   return [0].concat(_.tail(error.stderr.split('\n')));
+    // /Already on/
+    // /Previous HEAD position/
+    // /Your branch is up-to-date/
+}
+
+function submodulePullSuccessHandler(submodule, result) {
+    if(typeof result.stderr != 'undefined') {
+        return {status: 'success', submodule: submodule, message: result.stderr};
+    } else {
+        return {status: 'error', submodule: submodule, message: result.slice(1).join('\n')};
+    }
+}
+
+function handleResults(results) {
+    var errors = _.sortBy('submodule')(results)
+    .filter(_.flow(_.property('status'), _.eq('error')))
+    .map(function(error) {
+        return error.submodule + ':\n' + error.message;
+    });
+
+    if(!!errors.length) {
+        console.log(chalk.red.underline('\n\t=== ERRORS ==='));
+        errors.map(function(error) {errLog(error);});
+    }
+    console.log();
+    console.timeEnd(chalk.green('git submodule sync'));
+}
+
+function parseSubmodules(data) {
+    return data.match(/path\s=\s(.+)/g)
+               .map(function(str) {return str.replace('path = ', '');});
+}
+
+function checkoutAndPull(submodule) {
+    var gitCommand = 'git -C ./' + submodule;
+    var checkout = gitCommand +' checkout '+ BRANCH_FLAG;
+    var pull = gitCommand +' pull';
+
+    log('Pulling: ' + submodule + (BRANCH_FLAG ? ':' + BRANCH_FLAG : ''));
+
+    return exec(BRANCH_FLAG ? checkout + ' && ' + pull : pull)
+    .catch(submodulePullErrorHandler)
+    .then(submodulePullSuccessHandler.bind(null, submodule));
+}
+
+var pullSubmodules = _.flow(parseSubmodules, _.map(checkoutAndPull));
 
 function syncModules() {
     var gitModulesFile = path.resolve('.gitmodules');
+
     if(gitModulesFile) {
-        var promises = [];
-        var gitModules = fs.readFileAsync(gitModulesFile, 'utf8')
-        .then(function(data) {
-            return data.match(/path\s=\s(.+)/g)
-                       .map(function(v) {return v.replace('path = ', '');});
+        console.log('Indexing submodules...');
+        fs.readFileAsync(gitModulesFile, 'utf8')
+        .then(pullSubmodules)
+        .then(handleResults)
+        .catch({code: 'ENOENT'}, function() {
+            errLog('.gitmodules file not found');
         })
-        .then(function(data) {
-            data.forEach(function(v,k) {
-                var cmd = 'git -C ./'+ v +' checkout '+ BRANCH_FLAG +' && '+
-                          'git -C ./'+ v +' pull';
-                var gitPromise = exec(cmd)
-                .catch(function(error) {
-                    var errString = error.stderr.split('\n');
-                    errString.unshift(0);
-                    errString.join('\n');
-                    return errString;
-                    // /Already on/
-                    // /Previous HEAD position/
-                    // /Your branch is up-to-date/
-                })
-                .then(function(result) {
-                    if(typeof result.stderr != 'undefined') {
-                        return {status: 'success', station: v, message: result.stderr};
-                    } else {
-                        result.splice(0,2);
-                        result.pop();
-                        return {status: 'error', station: v, message: result.join('\n')};
-                    }
-                });
-
-                promises.push(gitPromise);
-                // console.log(promises);
-            });
-
-            Promise.all(promises)
-            .then(function(results) {
-                var errors = [];
-                var sortedResults = _.sortBy(results, 'station');
-                sortedResults.map(function(result) {
-                    // if(result.status === 'success' && !program.quiet) {console.log(result.message.trim(), "\t" + result.station.trim());}
-                    if(result.status === 'success') {console.log(result.message.trim(), "\t" + result.station.trim());}
-                    if(result.status === 'error') {errors.push(result.station + ":\n" + result.message);}
-                });
-                if(!!errors.length) {
-                    console.log(chalk.red.underline('\n\t=== ERRORS ==='));
-                    errors.map(function(error) {console.log(chalk.red(error));});
-                }
-                console.log();
-                console.timeEnd(chalk.green('git submodule sync'));
-            });
-        })
-        .catch({code: 'ENOENT'}, function(error) {
-            console.log(chalk.red('.gitmodules file not found'));
-        })
-        .catch(function(error) {
-            console.log(error);
-        });
+        .catch(log);
     }
 }
